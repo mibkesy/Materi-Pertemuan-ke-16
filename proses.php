032cf63c48900556997b746024138da17df708f3
M. Irfan Baari <mirfanbaari40@gmail.com>
2025-03-16 01:58:30 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,7 @@
+<?php
+echo '
+<form action="" method="post">
+<input type="button" value"submit">
+</form>';
+?>Terima kasih, telah melakukan pendaftaran Sidang Projek.. Silahkan menunggu konfirmasi selanjut nya.. Untuk jadiwal sidang Data, yang telah anda masukan adalah sebagai berikut di antara nya: NIM: 431910009, Nama: Steven Johnson, Proyek: Proyek-4, Judul Proyek: Aplikasi Pendataan Warga Lansia Desa Lanjut.. Sekian, terima kasih, dan salam.. 
+?>
